6724243cf9442c82ec0d8253fc069eb5384152d9
atmason90 <atmason90@gmail.com>
2022-05-21 15:54:09 -0700

    add Thoughts Schema to Thoughts model

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -31,6 +31,36 @@ const ReactionSchema = new Schema(
 
 const ThoughtsSchema = new Schema(
     {
-
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        username: {
+            type: String,
+            required: true 
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true 
+        },
+        id: false
     }
-)
+);
+
+ThoughtsSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thoughts = model('Thoughts', ThoughtsSchema);
+
+module.exports = Thoughts;
