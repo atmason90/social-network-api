29026838f0bc849e3802ffcebaa26b8fcaa53ff1
atmason90 <atmason90@gmail.com>
2022-05-21 15:00:18 -0700

    setup server.js with dependencies and mongoose connection

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+// connect mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+    useNewURLParser: true,
+    useUnifiedTopology: true
+});
+
+app.listen(PORT, () => console.log(`app listening on localhost:${PORT}`))
