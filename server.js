1c628c400816f2f1dc54b978afee975ebb78c190
atmason90 <atmason90@gmail.com>
2022-05-21 22:56:15 -0700

    edit server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.MONGODB_URI || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
